a80f4dff9f6ac7aa6819ec8eba9012f2b8ad0629
AlexPD93 <78558945+AlexPD93@users.noreply.github.com>
2021-12-16 15:34:37 -0600

    Created a search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-      </header>
+      <Dictionary />
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [searchWord, setSearchWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(searchWord);
+  }
+
+  function handleSearch(event) {
+    setSearchWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleSearch} />
+      </form>
+    </div>
+  );
+}
